48dab572bdec82406a5f232b022233f13bd92aff
Ginanjar Pamungkas <ginanjar69919082@gmail.com>
2020-06-16 10:56:12 +0700

    NEW MAPS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,78 +1,13 @@
 var controller = new ScrollMagic.Controller();
 var pinning = $('#flourishPinning')
 var pinningJakarta = $('#petaJakartaPinning')
-chart()
+// chart()
 // scrollmagic and flourish here
 pin = new ScrollMagic.Scene({triggerElement: '#pinTrigger'}).triggerHook(0).on("enter",(e)=>{pinning.css('position','fixed').css('top',0).css('bottom','unset')}).on("leave",(e)=>{pinning.css('position','absolute').css('top',0).css('bottom','unset')}).addTo(controller);
 endpin = new ScrollMagic.Scene({triggerElement: '#endpinTrigger'}).triggerHook(1).on("enter",(e)=>{pinning.css('position','absolute').css('top','unset').css('bottom',0)}).on("leave",(e)=>{pinning.css('position','fixed').css('top',0).css('bottom','unset')}).addTo(controller);
 scene1 = new ScrollMagic.Scene({triggerElement: "#step1"}).triggerHook(1).on("enter",(e)=>{$('#frame-chart').attr('src','https://flo.uri.sh/story/325435/embed#slide-0')}).on("leave",(e)=>{$('#frame-chart').attr('src','https://flo.uri.sh/story/325435/embed#slide-0')}).addTo(controller);
 scene2 = new ScrollMagic.Scene({triggerElement: "#step2"}).triggerHook(1).on("enter",(e)=>{$('#frame-chart').attr('src','https://flo.uri.sh/story/325435/embed#slide-1')}).on("leave",(e)=>{$('#frame-chart').attr('src','https://flo.uri.sh/story/325435/embed#slide-0')}).addTo(controller);
 scene3 = new ScrollMagic.Scene({triggerElement: "#step3"}).triggerHook(1).on("enter",(e)=>{$('#frame-chart').attr('src','https://flo.uri.sh/story/325435/embed#slide-2')}).on("leave",(e)=>{$('#frame-chart').attr('src','https://flo.uri.sh/story/325435/embed#slide-1')}).addTo(controller);
-// scrollmagic and map here
-var color = ['#ffffff', '#ffece7', '#ffd9cf', '#ffc5b6', '#fcb29f', '#f89f88', '#f38b72', '#ed775c', '#e66346', '#de4c31', '#d5321b', '#cc0000']
-pinJakarta = new ScrollMagic.Scene({triggerElement: '#pinJakartaTrigger'}).triggerHook(0).on("enter",(e)=>{pinningJakarta.css('position','fixed').css('top',0).css('bottom','unset')}).on("leave",(e)=>{pinningJakarta.css('position','absolute').css('top',0).css('bottom','unset')}).addTo(controller);
-endpinJakarta = new ScrollMagic.Scene({triggerElement: '#endpinJakartaTrigger'}).triggerHook(1).on("enter",(e)=>{pinningJakarta.css('position','absolute').css('top','unset').css('bottom',0)}).on("leave",(e)=>{pinningJakarta.css('position','fixed').css('top',0).css('bottom','unset')}).addTo(controller);
-
-sceneJakarta1 = new ScrollMagic.Scene({triggerElement: "#stepMap1"}).triggerHook(1)
-                    .on("enter",(e)=>{
-                        
-                        $('.kab-1').css('fill',color[1])
-                        $('.kab-2').css('fill',color[1])
-                        $('.kab-3').css('fill',color[0])
-                        $('.kab-4').css('fill',color[1])
-                        $('.kab-5').css('fill',color[0])
-                    })
-                    .on("leave",(e)=>{
-                        $('.kab').css('fill','#fff')
-                    }).addTo(controller);
-sceneJakarta2 = new ScrollMagic.Scene({triggerElement: "#stepMap2"}).triggerHook(1)
-                    .on("enter",(e)=>{
-                        
-                        $('.kab-1').css('fill',color[5])
-                        $('.kab-2').css('fill',color[5])
-                        $('.kab-3').css('fill',color[5])
-                        $('.kab-4').css('fill',color[5])
-                        $('.kab-5').css('fill',color[5])
-                    })
-                    .on("leave",(e)=>{
-                        $('.kab-1').css('fill',color[1])
-                        $('.kab-2').css('fill',color[1])
-                        $('.kab-3').css('fill',color[0])
-                        $('.kab-4').css('fill',color[1])
-                        $('.kab-5').css('fill',color[0])
-                    }).addTo(controller);
-sceneJakarta3 = new ScrollMagic.Scene({triggerElement: "#stepMap3"}).triggerHook(1)
-                    .on("enter",(e)=>{
-                        $('.kab-1').css('fill',color[10])
-                        $('.kab-2').css('fill',color[8])
-                        $('.kab-3').css('fill',color[10])
-                        $('.kab-4').css('fill',color[9])
-                        $('.kab-5').css('fill',color[10])
-                        $('.kabText').css('fill','#fff')
-                    })
-                    .on("leave",(e)=>{
-                        $('.kab-1').css('fill',color[5])
-                        $('.kab-2').css('fill',color[5])
-                        $('.kab-3').css('fill',color[5])
-                        $('.kab-4').css('fill',color[5])
-                        $('.kab-5').css('fill',color[5])
-                        $('.kabText').css('fill','#000')
-                    }).addTo(controller);
-sceneJakarta4 = new ScrollMagic.Scene({triggerElement: "#stepMap4"}).triggerHook(1)
-                    .on("enter",(e)=>{
-                        $('.kab-1').css('fill',color[11])
-                        $('.kab-2').css('fill',color[9])
-                        $('.kab-3').css('fill',color[11])
-                        $('.kab-4').css('fill',color[11])
-                        $('.kab-5').css('fill',color[11])
-                    })
-                    .on("leave",(e)=>{
-                        $('.kab-1').css('fill',color[10])
-                        $('.kab-2').css('fill',color[8])
-                        $('.kab-3').css('fill',color[10])
-                        $('.kab-4').css('fill',color[9])
-                        $('.kab-5').css('fill',color[10])
-                    }).addTo(controller);
 // heighchart here
 function chart(){
 	$.ajax({ 
@@ -166,9 +101,199 @@ var gJakartaMobile = svgMobile.append("g").attr("class", "jakartaMobile")
 var projectionMobile = d3.geoMercator().scale(180000).translate([-335128, -19135]);
 var pathMobile = d3.geoPath().projection(projectionMobile);
 
-d3.json("jakarta.json",function(json) {
-    jakarta = gJakarta.selectAll("path").data(json.features).enter().append("path").attr("d", path).attr("class", function(d) {return "kab kab-"+d.properties.ID;}).style("fill", '#fff').attr("stroke", "#000").attr("stroke-width", 0.2);
-    textJakarta = gJakarta.selectAll("text").data(json.features).enter().append("svg:text").text(function(d){return d.properties.NAME;}).attr("x", function(d){return path.centroid(d)[0]+10;}).attr("y", function(d){if(d.properties.NAME == 'Jakarta Timur'){return path.centroid(d)[1]-18};return  path.centroid(d)[1];}).attr("text-anchor","middle").attr('font-size','10pt').attr('font-weight','bold').attr('class','kabText');
-    jakartaMobile = gJakartaMobile.selectAll("path").data(json.features).enter().append("path").attr("d", pathMobile).attr("class", function(d) {return "kab kab-"+d.properties.ID;}).style("fill", '#fff').attr("stroke", "#000").attr("stroke-width", 0.2);
-    textJakartaMobile = gJakartaMobile.selectAll("text").data(json.features).enter().append("svg:text").text(function(d){return d.properties.NAME;}).attr("x", function(d){return pathMobile.centroid(d)[0]+10;}).attr("y", function(d){if(d.properties.NAME == 'Jakarta Timur'){return pathMobile.centroid(d)[1]-18};return  pathMobile.centroid(d)[1];}).attr("text-anchor","middle").attr('font-size','16pt').attr('font-weight','bold').attr('class','kabText');
+d3.json("jkt.json",function(json) {
+    jakarta = gJakarta.selectAll("path").data(json.features).enter().append("path").attr("d", path).attr("class", function(d) {return "kab kab-"+ string_to_slug(d.properties.kecamatan)}).style("fill", '#fff').attr("stroke", "#000").attr("stroke-width", 0.2);
+    // textJakarta = gJakarta.selectAll("text").data(json.features).enter().append("svg:text").text(function(d){return d.properties.name;}).attr("x", function(d){return path.centroid(d)[0]+10;}).attr("y", function(d){if(d.properties.name == 'Jakarta Timur'){return path.centroid(d)[1]-18};return  path.centroid(d)[1];}).attr("text-anchor","middle").attr('font-size','10pt').attr('font-weight','bold').attr('class','kabText');
+    jakartaMobile = gJakartaMobile.selectAll("path").data(json.features).enter().append("path").attr("d", pathMobile).attr("class", function(d) {return "kab kab-"+string_to_slug(d.properties.kecamatan)}).style("fill", '#fff').attr("stroke", "#000").attr("stroke-width", 0.2);
 });
+
+// scrollmagic and map here
+var color = ['#ffffff', '#ffe1d9', '#ffc3b3', '#f9a48e', '#f2866b', '#e76649', '#db4227', '#cc0000']
+pinJakarta = new ScrollMagic.Scene({triggerElement: '#pinJakartaTrigger'}).triggerHook(0).on("enter",(e)=>{pinningJakarta.css('position','fixed').css('top',0).css('bottom','unset')}).on("leave",(e)=>{pinningJakarta.css('position','absolute').css('top',0).css('bottom','unset')}).addTo(controller);
+endpinJakarta = new ScrollMagic.Scene({triggerElement: '#endpinJakartaTrigger'}).triggerHook(1).on("enter",(e)=>{pinningJakarta.css('position','absolute').css('top','unset').css('bottom',0)}).on("leave",(e)=>{pinningJakarta.css('position','fixed').css('top',0).css('bottom','unset')}).addTo(controller);
+
+sceneJakarta1 = new ScrollMagic.Scene({triggerElement: "#stepMap1"}).triggerHook(1)
+                    .on("enter",(e)=>{
+                        d3.csv('dataMaret.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+                    })
+                    .on("leave",(e)=>{
+                        $('.kab').css('fill','#fff')
+                    }).addTo(controller);
+sceneJakarta2 = new ScrollMagic.Scene({triggerElement: "#stepMap2"}).triggerHook(1)
+                    .on("enter",(e)=>{
+                        d3.csv('dataApril.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+                    })
+                    .on("leave",(e)=>{
+                        d3.csv('dataMaret.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+                    }).addTo(controller);
+sceneJakarta3 = new ScrollMagic.Scene({triggerElement: "#stepMap3"}).triggerHook(1)
+                    .on("enter",(e)=>{
+                        d3.csv('dataMei.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+                    })
+                    .on("leave",(e)=>{
+                        d3.csv('dataApril.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+                    }).addTo(controller);
+sceneJakarta4 = new ScrollMagic.Scene({triggerElement: "#stepMap4"}).triggerHook(1)
+                    .on("enter",(e)=>{
+                        d3.csv('dataJuni.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+                    })
+                    .on("leave",(e)=>{
+                        d3.csv('dataMei.csv',function(csv){
+							$.each(csv, function(id,v) {
+								if (v.positif > 351) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[7])
+								} else if (v.positif > 301) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[6])
+								} else if (v.positif > 251) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[5])
+								} else if (v.positif > 201) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[4])
+								} else if (v.positif > 151) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[3])
+								} else if (v.positif > 101) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[2])
+								} else if (v.positif > 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[1])
+								} else if (v.positif < 51) {
+									$('.kab-'+string_to_slug(v.kecamatan)).css('fill',color[0])
+								}
+							})
+						})
+					}).addTo(controller);
+					
+function string_to_slug(str) {
+	str = str.replace(/^\s+|\s+$/g, ""); // trim
+	str = str.toLowerCase();
+	var from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+	var to = "aaaaaaeeeeiiiioooouuuunc------";
+	for (var i = 0, l = from.length; i < l; i++) {
+	  str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+	}
+	str = str
+	  .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+	  .replace(/\s+/g, "-") // collapse whitespace and replace by -
+	  .replace(/-+/g, "-") // collapse dashes
+	  .replace(/^-+/, "") // trim - from start of text
+	  .replace(/-+$/, ""); // trim - from end of text
+	return str;
+}
